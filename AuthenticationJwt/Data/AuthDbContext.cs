305e69380a09f606a34208ede989098f3057ef4c
koimasshiro <ese.imhariagbe@gmail.com>
2023-03-14 20:47:19 +0100

    first commit

---
+++ AuthenticationJwt/Data/AuthDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AuthenticationJwt.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationJwt.Data
+{
+	public class AuthDbContext : DbContext
+	{
+		public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<User> Users => Set<User>();
+	}
+}
---
+++ AuthenticationJwt/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationJwt.Model
+{
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "First Name is required.")]
+		[MaxLength(30, ErrorMessage = "Maximum length is 30 characters.")]
+		public string? FirstName { get; set; }
+
+		[Required(ErrorMessage = "Last Name is required.")]
+		[MaxLength(30, ErrorMessage = "Maximum length is 30 characters.")]
+		public string? LastName { get; set; }
+
+		public string? Email { get; set; }
+
+		public byte[] PasswordHash { get; set; }
+
+		public byte[] PasswordSalt { get; set; }
+
+	}
+}
